39a3217a0ce4f58c9b223171e2ed02fdd9e71337
Bhuban Shrestha <bhubanshrestha12@gmail.com>
2018-12-08 22:21:25 +0545

    MVC Crud completed

--- ContactMVC/Controllers/ContactController.cs
+++ ContactMVC/Controllers/ContactController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Contact.CRUD;
 using System.Data;
+
 namespace ContactMVC.Controllers
 {
   public class ContactController : Controller
@@ -37,7 +38,71 @@ namespace ContactMVC.Controllers
     [HttpGet]
     public ActionResult Add()
     {
-      return View();
+      return View(new ContactMVC.Models.ContactPerson());
+    }
+
+    [HttpPost]
+    public ActionResult Add(ContactMVC.Models.ContactPerson person)
+    {
+      if (ModelState.IsValid)
+      {
+        Contact.Model.Person p = person;
+        p.ImageName = GetPictureName(person.ProfilePicture);
+        PersonCrud personCrud = new PersonCrud();
+        personCrud.Add(p);
+        return RedirectToAction("Add");
+      }
+      return View(person);
+    }
+
+    [HttpGet]
+    public ActionResult Edit(int id)
+    {
+      PersonCrud personCrud = new PersonCrud();
+      DataTable tbl = personCrud.GetContactById(id);
+      Models.ContactPerson person = new Models.ContactPerson().GetPerson(tbl.Rows[0]);
+      return View(person);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Models.ContactPerson person)
+    {
+      if(ModelState.IsValid)
+      {
+        Contact.Model.Person p = person;
+        p.ImageName = GetPictureName(person.ProfilePicture);
+        PersonCrud personCrud = new PersonCrud();
+        personCrud.Update(p, person.Id);
+        return RedirectToAction("Index");
+      }
+      return View(person);
+    }
+
+    [HttpGet]
+    public ActionResult Search(string q)
+    {
+      PersonCrud personCrud = new PersonCrud();
+      DataTable tbl = personCrud.GetAllContact();
+      if (!string.IsNullOrEmpty(q))
+      {
+        string filter = string.Format("FullName Like '%{0}%' OR PrimaryEmail like '%{0}%' OR Company Like '%{0}%'", q);
+        DataRow[] rows = tbl.Select(filter);
+        return View(rows);
+      }
+      DataRow[] rows1 = tbl.Select();
+      return View(rows1);
+    }
+
+    private string GetPictureName(HttpPostedFileBase pic)
+    {
+      if (pic != null && pic.ContentLength > 0)
+      {
+        string fileName = Guid.NewGuid().ToString("N") + ".png";
+        string path = Server.MapPath(@"~\Content\Images\Profile\" + fileName);
+        pic.SaveAs(path);
+        return fileName;
+      }
+      return "";
     }
   }
 }
---
+++ ContactMVC/Models/ContactPerson.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Contact.Model;
+using System.Data;
+
+namespace ContactMVC.Models
+{
+  public class ContactPerson: Person
+  {
+    public HttpPostedFileBase ProfilePicture { get; set; }
+
+    public ContactPerson GetPerson(DataRow row)
+    {
+      if(row != null)
+      {
+        this.FullName = row["FullName"].ToString();
+        this.Id = Convert.ToInt32(row["Id"]);
+        this.Address = row["Address"].ToString();
+        this.PrimaryEmail = row["PrimaryEmail"].ToString();
+        this.GroupId = Convert.ToInt32(row["GroupId"]);
+        this.Mobile1 = row["Mobile1"].ToString();
+        this.Mobile2 = row["Mobile2"].ToString();
+        this.Relation = row["Relation"].ToString();
+        this.BirthDay = row["Birthday"] == DBNull.Value? DateTime.MinValue: Convert.ToDateTime(row["Birthday"]);
+        this.Company = row["Company"].ToString();
+        this.Title = row["Title"].ToString();
+        this.Website = row["Website"].ToString();
+        this.ProfEmail = row["ProfEmail"].ToString();
+        this.Telephone1 = row["Telephone1"].ToString();
+        this.Telephone2 = row["Telephone2"].ToString();
+        this.ProfMobile1 = row["ProfMobile1"].ToString();
+        this.ProfMobile2 = row["ProfMobile2"].ToString();
+        this.Fax = row["Fax"].ToString();
+        this.Note = row["Note"].ToString();
+        this.Skype = row["Skype"].ToString();
+        this.WindowsLive = row["windowsLive"].ToString();
+        this.Yahoo = row["Yahoo"].ToString();
+        this.Hangout = row["Hangout"].ToString();
+        this.Gmail = row["Gmail"].ToString();
+        this.Facebook = row["Facebook"].ToString();
+        this.ImageName = row["ProfileImage"].ToString();
+      }
+      return this;
+    }
+  }
+
+
+}
